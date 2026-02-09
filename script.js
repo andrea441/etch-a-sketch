8d33f3060dbddc6ebadba6795add30b0240852b5
Andrea Burciaga <174824899+andrea441@users.noreply.github.com>
2026-02-09 14:24:20 -0600

    Add a functionality to paint the cells using the cursor

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+function drawGrid(size) {
+    grid.textContent = '';
+
+    grid.style.setProperty('--size', size);
+
+    totalSquares = size ** 2;
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('cell');
+        grid.appendChild(square);
+    }
+}
+
+// Select the grid container
 const grid = document.querySelector('#grid');
 
+// Create the grid
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('cell');
     grid.appendChild(square);
 }
+
+// Add functionality to the cells
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add('colored');
+    });
+})
+
+// Add functionality to the button
+const sizeBtn = document.querySelector('#size');
+sizeBtn.addEventListener('click', () => {
+    size = prompt('Choose the new grid size: ');
+    drawGrid(size);
+})
